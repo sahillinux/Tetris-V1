9cb852a983d033452e814da617d1032dbc7ecca7
sahillinux <sahil.jet786@gmail.com>
2024-07-19 17:13:17 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-shakil-buckets" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "EKS/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
